508275e088e00b380f368d81460787ed37c1fa09
sd <leowest1921@gmail.com>
2022-05-17 16:58:40 +0800

    add

---
+++ bin/check.ts
@@ -0,0 +1,24 @@
+/**
+ * 单线程模式
+ */
+import { Command } from "commander";
+import * as check from "../src/utils/check";
+const program = new Command();
+let appParams: any = program //
+   .option("-host, --host [value]", "代理主机", "127.0.0.1") //
+   .option("-port, --port [value]", "代理端口", "6379") //
+   .option("-secret, --secret [value]", "密钥", "")
+   .option("-u, --username [value]", "用户", "") //
+   .option("-p, --password [value]", "密码", "") //
+   .option("-protocol, --protocol [value]", "密码", "socks5") //
+   .parse(process.argv)
+   .opts();
+
+console.info("args", appParams);
+(async () => {
+   let checked = await check.check(appParams).catch((err) => {
+      console.info("check proxyn error", err.message);
+      return false;
+   });
+   console.info("check proxy", checked);
+})();
